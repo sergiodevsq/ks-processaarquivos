11f77376696c48a7a306e3a839c78e388d86ff6d
Sergio Queiroz <sergio@sq.dev.br>
2023-09-10 14:22:48 -0300

    Testando classe de domínio Arquivo.java

--- processaarquivo-backend/src/main/java/br/com/queiroz/processadorxml/controllers/LeitorArquivosController.java
+++ processaarquivo-backend/src/main/java/br/com/queiroz/processadorxml/controllers/LeitorArquivosController.java
@@ -1,8 +1,13 @@
 package br.com.queiroz.processadorxml.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.queiroz.processadorxml.models.Arquivo;
 
 
 
@@ -11,8 +16,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class LeitorArquivosController {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String inserirDados() {
-		return "Rest está funcionando";
+	public List<Arquivo> inserirDados() {
+		
+		Arquivo arq1 = new Arquivo(1L, "Arquivo001.xml");
+		Arquivo arq2 = new Arquivo(2L, "Arquivo002.xml");
+		
+		
+		List<Arquivo> arquivos = new ArrayList<>();
+		
+		
+		arquivos.add(arq1);
+		arquivos.add(arq2);
+		
+		return arquivos;
 	}
 	
 }
--- processaarquivo-backend/src/main/java/br/com/queiroz/processadorxml/models/Arquivo.java
+++ processaarquivo-backend/src/main/java/br/com/queiroz/processadorxml/models/Arquivo.java
@@ -1,8 +1,58 @@
 package br.com.queiroz.processadorxml.models;
 
-public class Arquivo {
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Arquivo implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
 	
 	private Long id;
 	private String nomeFisicoArquivo;
+	
+	public Arquivo() {}
+
+	public Arquivo(Long id, String nomeFisicoArquivo) {
+		this.id = id;
+		this.nomeFisicoArquivo = nomeFisicoArquivo;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNomeFisicoArquivo() {
+		return nomeFisicoArquivo;
+	}
+
+	public void setNomeFisicoArquivo(String nomeFisicoArquivo) {
+		this.nomeFisicoArquivo = nomeFisicoArquivo;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Arquivo other = (Arquivo) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
+	
+	
 	
 }
